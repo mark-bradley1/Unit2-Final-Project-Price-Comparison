580f2f9489413fc42a7881fa05e8c47758185e1a
mark-bradley1 <mbradle15@gmail.com>
2026-03-04 13:50:11 -0600

    fixed className issue

--- price-comparison-frontend/Grocery-Price-Comparison/src/components/RemoveBtn.jsx
+++ price-comparison-frontend/Grocery-Price-Comparison/src/components/RemoveBtn.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const RemoveBtn = ({ id, type = "button", label, onClick, className }) => {
   return (
     <button id={id} type={type} onClick={onClick} className={className}>
-    <span class="remove-btn-icon">
-        <i class="fa-solid fa-circle-xmark"></i>
+    <span className="remove-btn-icon">
+        <i className="fa-solid fa-circle-xmark"></i>
     </span>
       {label}
     </button>
