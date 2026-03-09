84a63d7a91abab4a77146bfed97c516e904c4d69
mark-bradley1 <mbradle15@gmail.com>
2026-03-09 18:11:28 -0500

    Added ContactForm model and coded the model.

---
+++ price-comparison-backend/Price-Comparison/src/main/java/com/example/Price_Comparison/models/ContactForm.java
@@ -0,0 +1,77 @@
+package com.example.Price_Comparison.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class ContactForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+    private LocalDateTime submittedAt;
+
+    @OneToOne(mappedBy = "contactForm", cascade = CascadeType.ALL)
+    private List<ContactForm>  response;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getSubmittedAt() {
+        return submittedAt;
+    }
+
+    public void setSubmittedAt(LocalDateTime submittedAt) {
+        this.submittedAt = submittedAt;
+    }
+
+    public List<ContactForm> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<ContactForm> response) {
+        this.response = response;
+    }
+}
