7e6cd94afbffbba9d9f8b14ce1de507558ae3afa
mark-bradley1 <mbradle15@gmail.com>
2026-03-09 20:40:27 -0500

    Coded ShoppingCartRepository

--- price-comparison-backend/Price-Comparison/src/main/java/com/example/Price_Comparison/Repositories/ShoppingCartRepository.java
+++ price-comparison-backend/Price-Comparison/src/main/java/com/example/Price_Comparison/Repositories/ShoppingCartRepository.java
@@ -1,4 +1,7 @@
 package com.example.Price_Comparison.Repositories;
 
-public interface ShoppingCartRepository {
+import com.example.Price_Comparison.models.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
 }
