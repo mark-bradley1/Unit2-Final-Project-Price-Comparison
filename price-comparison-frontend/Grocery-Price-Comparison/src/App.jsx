f1b627a265db5e9390f7ee1bc3cced8f2903cde1
mark-bradley1 <mbradle15@gmail.com>
2026-03-11 11:36:44 -0500

    Made changes after testing. Cart is now populating in database

--- price-comparison-frontend/Grocery-Price-Comparison/src/App.jsx
+++ price-comparison-frontend/Grocery-Price-Comparison/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/layout/Header";
